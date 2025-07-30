bf5600125fedc53d4b0f45978f6ba8687c4295fc
KIILL <nathanielias13@gmail.com>
2025-07-30 06:48:42 -0700

    Create strategy_selector.py

---
+++ strategy_selector.py
@@ -0,0 +1,4 @@
+def select_best_signal(signals: dict) -> str:
+    priority = {"breakout": 3, "dip": 2, "sideways": 1, "error": 0}
+    sorted_signals = sorted(signals.items(), key=lambda x: priority.get(x[1], 0), reverse=True)
+    return sorted_signals[0][0] if sorted_signals else None
